f6b2dc79f86658f2be616fc994e4693c9c727377
Anton de Vries <anton.devries@student.uni-luebeck.de>
2020-01-15 16:02:37 +0100

    pedal works

--- Pedal.js
+++ Pedal.js
@@ -2,37 +2,29 @@
 // Root element 
 const root = document.getElementsByTagName('svg')[0];
 var img = document.getElementById("pedalImg");
+var svg = document.getElementById("svg");
+var bBox = svg.getBBox();
+var bBoxH = bBox.height.toString();
+var bBoxW = bBox.width.toString();
+var canvas = document.getElementById('pedalcanvas');
+var context = canvas.getContext('2d');
 
-document.PedalUi = function(){
-    //MainObj
-   const pedalUi = {
-       root: root
-   };
-    
-    return PedalUi;
-}
-document.drawPedal = function(){
-    var canvas = document.getElementById('pedalcanvas');
-    var context = canvas.getContext('2d');
 
-    
-    
-    img.onload = function(){
-        context.drawImage(img , 200 , 0 , );
-        console.log('hier');
-        //console.log()
-    }
-    img.src = "test.svg";   
-    //context.drawImage(pedalSvg);  
-    //context.beginPath();
-    //context.moveTo(21038,2728);
-    //context.lineTo(18806,835);
-    //context.stroke();
-}
+
+
 
 document.rotatePedal = function(value){
-    
-    img.style.transform = "rotate(180 deg)";
-    console.log("rotatePedal");
-    
+    var angle = 0;
+    var multiplikator = -53;
+
+    var angle = value * multiplikator;
+
+    if (value == 1){
+        angle = -53;
+        
+    }
+
+    img.style.transform = `rotate(${angle}deg) `;
+    console.log(angle);
 }
+
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 angular.module('beamng.apps')
-.directive('pedal', ['StreamsManager', 'UiUnits' ,function (StreamsManager, UiUnits) {
+.directive('pedal', ['StreamsManager', 'UiUnits', 'bngApi' ,function (StreamsManager, UiUnits, bngApi) {
   return {
     template:  '<iframe src="modules/apps/Pedal/Pedal.html" style="width:100%; height:100%;" frameborder="0"></iframe>',
     replace: true,
@@ -22,13 +22,19 @@ angular.module('beamng.apps')
 
       element.on('load', function(){
         iframe = element[0].contentDocument;
-        iframe.drawPedal();
+        //iframe.drawPedal();
+        console.log('hier10'); 
 
       });
 
       scope.$on('streamsUpdate', function (event, streams) {
+        //iframe.drawBereich();
+        //iframe.rotatePedal(streams.engineInfo[0]);
+       
+        iframe.rotatePedal(streams.electrics.engineThrottle);
+        console.log(`throttle state ${streams.electrics.throttle}`);
+        console.log('overflow:hidden4');
         
-        iframe.rotatePedal(streams.engineInfo[0]);
         
       });
     }
